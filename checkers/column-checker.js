def34b96c1ae896ab5312b01624b5e304f02e984
Greg <gregthompson27@gmail.com>
2021-01-20 01:56:31 -0600

    (add) files for row and column algorithms

---
+++ checkers/column-checker.js
@@ -0,0 +1,16 @@
+const checkColumn = (grid, columnNumber) => {
+  let flag = true;
+
+  return flag;
+};
+
+const checkAllColumns = (grid) => {
+  let flag = true;
+
+  return flag;
+};
+
+module.exports = {
+  checkColumn,
+  checkAllColumns,
+};
---
+++ checkers/row-checker.js
@@ -0,0 +1,16 @@
+const checkRow = (grid, rowNumber) => {
+  let flag = true;
+
+  return flag;
+};
+
+const checkAllRows = (grid) => {
+  let flag = true;
+
+  return true;
+};
+
+module.exports = {
+  checkRow,
+  checkAllRows,
+};
