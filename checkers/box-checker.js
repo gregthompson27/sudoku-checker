dba8d6da5e817b01ee189bc5bfc10b10ea9ad7fe
Greg <gregthompson27@gmail.com>
2021-01-21 13:20:51 -0600

    (add) box-checker

--- checkers/box-checker.js
+++ checkers/box-checker.js
@@ -1,13 +1,25 @@
-const checkBox = (grid, boxNumber) => {
-  let flag = true;
+const { counter } = require('./helpers/base-obj.js');
 
-  return flag;
+const checkBox = (grid, boxNumber) => {
+  const up = Math.floor((boxNumber - 1) / 3) * 3
+  const across = ((boxNumber - 1) % 3) * 3
+  const boxContents = [];
+  for (let i = up; i < up + 3; i++) {
+    for (let j = across; j < across + 3; j++) {
+      boxContents.push(grid[i][j]);
+    }
+  }
+
+  return counter(boxContents);
 };
 
 const checkAllBoxes = (grid) => {
-  let flag = true;
+  const boxResults = [];
+  for (let i = 1; i < 10; i++) {
+    boxResults.push(checkBox(grid, i));
+  }
 
-  return flag;
+  return !boxResults.includes(false);
 };
 
 module.exports = {
